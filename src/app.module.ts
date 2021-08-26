27cbb6fc7f4913415a42fdf7fd1aef3e4ea2e567
abebelagua <abebelagua@gmail.com>
2021-08-26 23:47:43 +0200

    ConfigModule and MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        HOST: Joi.string().default('localhost'),
+        PORT: Joi.number().default(4000),
+        MONGO_DB_HOST: Joi.string().default('localhost'),
+        MONGO_DB_PORT: Joi.number().default(27017),
+        HOMONGO_DBST: Joi.string().default('testproyect'),
+        MONGO_DB_CONNECTION: Joi.string().default(
+          'mongodb://localhost:27017/testproyect',
+        ),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_DB_CONNECTION'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3030);
+
+  const configService = app.get(ConfigService);
+
+  const host = configService.get('HOST');
+  const port = configService.get('PORT');
+
+  await app.listen(process.env.PORT);
+
+  Logger.log(`Server started running on http://${host}:${port}`, 'NestJS');
 }
 bootstrap();
